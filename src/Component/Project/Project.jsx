ff5c74315816a29446b0448dd91b9a404206a8b6
4512yasir <yasiryaska1@gmail.com>
2022-12-07 16:03:28 +0300

    jj

--- src/Component/Project/Project.jsx
+++ src/Component/Project/Project.jsx
@@ -3,6 +3,8 @@ import './Project.scss'
 import tip from '../../Assets/tip.png'
 import project from '../../Assets/Screenshot (5).png'
 import projects from '../../Assets/Screenshot (4).png'
+import todo from '../../Assets/todo.png'
+import api from '../../Assets/api.png'
 
 
 function Project() {
@@ -25,7 +27,7 @@ function Project() {
         <p className='short-des'><em>This project was created using Html,css and javascript</em></p>
         <p className='description'>This is a calculator which took 3 days to build</p>
         <div className="buttons">
-   <a href="https://calator.netlify.app/" target='_blank'><button>GO LIVE</button></a>     
+   <a href="https://calator.netlify.app/" ><button>GO LIVE</button></a>     
         </div>
       </div>
  </div>
@@ -37,7 +39,7 @@ function Project() {
         <p className='short-des'><em>This project was created using HTML,JAVASCRIPT,CSS</em></p>
         <p className='description'>This is a animated  landing page which took 1 day to build</p>
            <div className="buttons">
-   <a href="https://ysc1.netlify.app/" target='_blank'><button>GO LIVE</button></a>     
+   <a href="https://ysc1.netlify.app/" ><button>GO LIVE</button></a>     
         </div>
       </div>
    
@@ -51,7 +53,33 @@ function Project() {
         <p className='short-des'><em>This project was created using react and sass only</em></p>
         <p className='description'>This is a clone  of whatsapp which took 1 weeks to build</p>
         <div className="buttons">
-   <a href="https://tip-calculator-2-3.netlify.app/" target='_blank'><button>GO LIVE</button></a>     
+   <a href="https://tip-calculator-2-3.netlify.app/" ><button>GO LIVE</button></a>     
+        </div>
+      </div>
+ </div>
+  <div className="projects">
+      <img src={todo} alt="" />
+ 
+      <div className="project-text">
+           
+        <h1>TO-DO LIST</h1>
+        <p className='short-des'><em>This project was created using HTML CSS AND JS only</em></p>
+        <p className='description'>This is a clone  of whatsapp which took 1 weeks to build</p>
+        <div className="buttons">
+   <a href="https://work-manager.netlify.app/" ><button>GO LIVE</button></a>     
+        </div>
+      </div>
+ </div>
+  <div className="projects">
+      <img src={api} alt="" />
+ 
+      <div className="project-text">
+           
+        <h1>MOVIE-API</h1>
+        <p className='short-des'><em>This project was created using HTML CSS AND JS only</em></p>
+        <p className='description'>This is a api project</p>
+        <div className="buttons">
+   <a href="https://work-manager.netlify.app/" ><button>GO LIVE</button></a>     
         </div>
       </div>
  </div>
